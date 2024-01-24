1d2f68db85084a5e98c9223c90fe66bfb026621e
Edmondo Castoldi <edmondo.castoldi@protonmail.com>
2024-01-24 16:13:28 +0100

    fixed ci.yml according to assignment

--- main_test.go
+++ main_test.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-		"strings"
-    "testing"
+	"strings"
+	"testing"
 )
 
 func TestAddParseTimeParam(t *testing.T) {
-    tests := []struct {
-        name         string
-        input        string
-        wantContains string
-        wantErr      bool
-    }{
-        {"With Scheme", "http://example.com", "parseTime=true", false},
-        {"Without Scheme", "example.com", "parseTime=true", false},
-        {"Invalid URL", "this is not a URL", "", true},
-    }
+	tests := []struct {
+		name         string
+		input        string
+		wantContains string
+		wantErr      bool
+	}{
+		{"With Scheme", "http://example.com", "parseTime=true", false},
+		{"Without Scheme", "example.com", "parseTime=true", false},
+		{"Invalid URL", "this is not a URL", "", true},
+	}
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            got, err := addParseTimeParam(tt.input)
-            if (err != nil) != tt.wantErr {
-                t.Errorf("addParseTimeParam() error = %v, wantErr %v", err, tt.wantErr)
-                return
-            }
-            if !tt.wantErr && !strings.Contains(got, tt.wantContains) {
-                t.Errorf("addParseTimeParam() = %v, want %v", got, tt.wantContains)
-            }
-        })
-    }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := addParseTimeParam(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("addParseTimeParam() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr && !strings.Contains(got, tt.wantContains) {
+				t.Errorf("addParseTimeParam() = %v, want %v", got, tt.wantContains)
+			}
+		})
+	}
 }
